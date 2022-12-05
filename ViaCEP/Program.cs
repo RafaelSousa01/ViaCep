9ef8c6dd609e47b6ae965fbf3f38334f41a61308
Rafael Sousa <rafaelsousa.s@hotmail.com>
2022-12-04 22:11:54 -0300

    Implementado salvar arquivo json

--- ViaCEP/Program.cs
+++ ViaCEP/Program.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Globalization;
-using System.Runtime.ConstrainedExecution;
-using RestSharp;
+﻿using RestSharp;
+using System.IO;
+
 
 internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
         bool menu = true;
 
         while (menu)
         {
             Console.WriteLine("----------BUSCA DE ENDEREÇO------------");
-            Console.WriteLine("Você sabe seu CEP? Se sim, digite 's'. ");
-            Console.WriteLine("Digite 'n' para caso não saber seu CEP");
-            char escolha1 = char.Parse(Console.ReadLine());
-
-            var client = new RestClient($"https://viacep.com.br");
-
-            if (escolha1 == 's')
+            try
             {
-                Console.WriteLine("Digite um CEP valido:");
-
+                Console.Write("Digite um CEP válido: ");
                 string cep = Console.ReadLine();
+                Console.WriteLine();
+
+
+                var client = new RestClient($"https://viacep.com.br");
 
                 var request = new RestRequest($"/ws/{cep}/json/", Method.Get);
 
@@ -31,34 +27,26 @@ internal class Program
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(response.Content);
                 Console.ForegroundColor = ConsoleColor.White;
+
+                string targetPath = $@"C:\Users\rafae\Documents\AJD\TreinoAPI\APIViaCep\CepPesquisado\{cep}.json";
+                File.WriteAllText(targetPath, response.Content);
+                Console.WriteLine();
+                Console.WriteLine("Deseja encerrar o programa? Digite 's' \n Para realizar outra busca digite 'n'");
+                char encerraPrograma = char.Parse(Console.ReadLine());
+                if (encerraPrograma == 's')
+                {
+                    break;
+                }
+                Console.Clear();
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("Digite a UF (Obrigatório): ");
-                string uf = Console.ReadLine();
-                Console.WriteLine("Digite a Cidade (Obrigatório): ");
-                string cidade = Console.ReadLine();
-                Console.WriteLine("Digite o endereço: \n (Pode ser o endereço completo ou apenas um nome que lembrar)");
-                string endereço = Console.ReadLine();
-
-                var request = new RestRequest($"/ws/{uf}/{cidade}/{endereço}/json/", Method.Get);
-
-                var response = client.Get(request);
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(response.Content);
-                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Digite um valor com 8 digitos! Pressione Enter para continuar");
+                Console.ReadLine();
             }
-            Console.WriteLine("Deseja encerrar o programa? Digite 's' \n Para realizar outra busca digite 'n'");
-            char encerraPrograma = char.Parse(Console.ReadLine());
-            if (encerraPrograma == 's')
-            {
-                break;
-            }
-            Console.Clear();
         }
+
         Console.WriteLine("Programa encerrado!");
 
-        Console.ReadLine();
     }
 }
